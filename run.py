cae17d56f3dad26beb6a7d467f0313b063c1c3a4
Harkirat Singh <harkirat1892@gmail.com>
2019-01-05 03:16:15 +0530

    Better and randomized booking attempts

--- run.py
+++ run.py
@@ -1,22 +1,20 @@
-import requests
 import time
+from random import randint
+
+import arrow
+import requests
 
 import config
 import preferences
-
-from random import randint
 from auth import cookie, api_key, user_agent, center_id
-
 
 headers = {"accept": "application/json",
            "apikey": api_key,
            "appversion": "7",
-           "referer": "https://www.cure.fit/cult/classbooking/{}?centerId={}&pageType=classbooking".format(
-               center_id,
-               center_id
-           ),
+           "referer": "https://www.cure.fit/cult/classbooking?pageFrom=cultCLP&pageType=classbooking",
            "user-agent": user_agent,
            "dnt": "1",
+           "osname": "browser",
            "origin": "https://www.cure.fit",
            "content-type": "application/json",
            "cookie": cookie}
@@ -26,7 +24,7 @@ def get_upcoming_classes():
     res = requests.get(config.classes_api_url.format(center_id), headers=headers)
 
     res_json = res.json()
-    # print("res_json: ", res_json)
+    print("res_json: ", res_json, flush=True)
 
     return res_json['classByDateList']
 
@@ -37,105 +35,142 @@ def get_classes_for_today():
     return class_by_date_list[0]
 
 
+def get_classes_for_upcoming_days():
+    return get_upcoming_classes()
+
+
 def get_preferred_availability_status():
-    classes_today = get_classes_for_today()
-    print("Looking for preferred classes for ", classes_today['id'])
+    upcoming_days_and_classes = get_classes_for_upcoming_days()
+    count_of_upcoming_days = len(upcoming_days_and_classes)
 
-    # print("classes_today: ", classes_today)
-    time_class_list = classes_today['classByTimeList']
+    booked_count = 0
+    for classes_on_day in upcoming_days_and_classes:
+        print("\n\nLooking for preferred classes for ", classes_on_day['id'], flush=True)
 
-    # Get time preference
-    preferred_times = preferences.preferred_times
-    preferred_classes = preferences.preferred_classes
+        # print("classes_on_day: ", classes_on_day, flush=True)
+        time_class_list = classes_on_day['classByTimeList']
 
-    if preferred_times:
-        print("Preference for time found: ", preferred_times)
+        # Get time preference
+        preferred_times = preferences.preferred_times
+        preferred_classes = preferences.preferred_classes
 
-    time_class_map = {}
-    for time_class in time_class_list:
-        time_class_map[time_class['id']] = time_class
+        if preferred_times:
+            print("Preference for time found: ", preferred_times, flush=True)
 
-    for preferred_time in preferred_times:
-        print("Looking for your favourite classes at ", preferred_time)
-        # print("time_class_map is: ", time_class_map)
+        time_class_map = {}
+        for time_class in time_class_list:
+            time_class_map[time_class['id']] = time_class
 
-        classes_today_at_preferred_time = time_class_map[preferred_time]
+        for preferred_time in preferred_times:
+            print("Looking for your favourite classes at ", preferred_time, flush=True)
+            # print("time_class_map is: ", time_class_map)
 
-        if classes_today_at_preferred_time['disableGroup'] == True:
-            print("The time slot is already booked. Skipping...")
-            continue
+            classes_today_at_preferred_time = time_class_map.get(preferred_time)
+            if not classes_today_at_preferred_time:
+                booked_count += 1
+                print("No classes at preferred time found, skipping to next preferred time", flush=True)
+                continue
 
-        available_classes_id_map = {}
-        for scheduled_class in classes_today_at_preferred_time['classes']:
-            workout_id = scheduled_class['id']
-            workout_state = scheduled_class['state']
+            if classes_today_at_preferred_time['disableGroup'] == True:
+                print("The time slot is already booked. Skipping...", flush=True)
+                booked_count += 1
+                continue
 
-            workout_name = scheduled_class['workoutName']
+            available_classes_id_map = {}
+            for scheduled_class in classes_today_at_preferred_time['classes']:
+                workout_id = scheduled_class['id']
+                workout_state = scheduled_class['state']
 
-            if scheduled_class['availableSeats'] == 0:
-                print("Whooaa! {} is not available!".format(workout_name))
+                workout_name = scheduled_class['workoutName']
 
-                if workout_name not in preferred_classes:
-                    print("Doesn't matter, you don't care about that! Meh!")
-                # else:
-                #     print("{} not available right now".format(workout_name))
-            else:
-                available_classes_id_map[workout_name] = workout_id
+                if scheduled_class['availableSeats'] == 0:
+                    print("Whooaa! {} is not available!".format(workout_name), flush=True)
 
-        print("Available classes for {} are: ".format(preferred_time, available_classes_id_map))
-
-        # Now we know available classes for this day
-        # Let's walk through preferences
-        class_booked = False
-        for preferred_class_name in preferred_classes:
-            if not class_booked:
-                if preferred_class_name in available_classes_id_map:
-                    print("Booking your preferred class of {} for {}".format(
-                        preferred_class_name,
-                        preferred_time
-                    ))
-
-                    class_booked = book_class_using_id(available_classes_id_map[preferred_class_name])
-
-                    if class_booked:
-                        print("{} has been booked! My purpose is over. So long and thanks for all the fish..".format(
-                            preferred_class_name))
+                    if workout_name not in preferred_classes:
+                        print("Doesn't matter, you don't care about {}! Meh!".format(workout_name), flush=True)
+                    # else:
+                    #     print("{} not available right now".format(workout_name))
                 else:
-                    print("{} is not available".format(preferred_class_name))
-            else:
-                print("Class already booked! WTF am I running here for? Fix me!")
+                    available_classes_id_map[workout_name] = workout_id
 
-        return class_booked
+            # print("Available classes for {} are: ".format(preferred_time, available_classes_id_map), flush=True)
+
+            # Now we know available classes for this day
+            # Let's walk through preferences
+            class_booked = False
+            for preferred_class_name in preferred_classes:
+                if not class_booked:
+                    if preferred_class_name in available_classes_id_map:
+                        print("Booking your preferred class of {} for {}".format(
+                            preferred_class_name,
+                            preferred_time
+                        ), flush=True)
+
+                        class_booked = book_class_using_id(available_classes_id_map[preferred_class_name])
+
+                        if class_booked:
+                            print("{} has been booked! My purpose is over. So long and thanks for all the fish..".format(
+                                preferred_class_name), flush=True)
+                            booked_count += 1
+                            break
+                    else:
+                        print("{} is not available".format(preferred_class_name), flush=True)
+                else:
+                    print("Class already booked! WTF am I running here for? Fix me!", flush=True)
+
+    return bool(count_of_upcoming_days == booked_count)
 
 
 def book_class_using_id(class_id):
-    print("I was going to book, but remembered that I haven't been implemented yet :/ Class ID: {}".format(class_id))
-    # To really book or to just do a dry run
+    #print("I was going to book, but remembered that I haven't been implemented yet :/ Class ID: {}".format(class_id))
     book = True
 
     if book:
         booking_url = config.book_class_url.format(class_id)
 
         booking_status = requests.post(booking_url, headers=headers)
-        print("Class booking status: {}".format(booking_status.json()))
+        print("Class booking status: {}".format(booking_status.json()), flush=True)
 
     return class_id
 
-def try_fav_booking():
-    is_booked = False
 
+def try_in_randomized_time():
     i = 0
-    while not is_booked:
+    while True:
         is_booked = get_preferred_availability_status()
 
-        sleep_time = randint(5, 20)*60
-        print("\n\nSleeping for {} minutes\n\n\n\n".format(sleep_time//60))
-        time.sleep(sleep_time)
-        i += 1
+        if not is_booked:
+            sleep_time = randint(5, 20)*60
+            print("\n\nSleeping for {} minutes\n\n\n\n".format(sleep_time // 60), flush=True)
+            time.sleep(sleep_time)
+            i += 1
+        else:  # All sorted, booked!!!!
+            now = arrow.now()
+            t = now.time()
 
-    print("Class booked! {} attempts made. Do good with your health!\nExiting..".format(i))
+            arrow_ts_near_next_schedule = arrow.now().replace(hour=21, minute=randint(20, 59))
+            if t.hour > 21:
+                arrow_ts_near_next_schedule = arrow.now().replace(days=1, hour=21, minute=randint(20, 59))
+
+            secs_to_next_schedule = arrow_ts_near_next_schedule.timestamp - int(time.time())
+            sleep_time = secs_to_next_schedule
+
+            # Aiming for the new day that is yet to come! Added after 10pm
+            if 19 < t.hour < 23:
+                # Shift gear to check more frequently
+                sleep_time = randint(15, 30) * 60
+
+            # if t.hour >= 22, Booked for the new day too!
+            print("Class booked! {} attempts made. Do good with your health!\nExiting..".format(i), flush=True)
+
+            print("\n\nSleeping for {} minutes. Gonna wake up at: {}\n\n".format(sleep_time // 60,
+                                                                             arrow_ts_near_next_schedule),
+                  flush=True)
+            time.sleep(sleep_time)
+            print("\n\nTrying to book next day, Master!\n\n\n\n", flush=True)
+            i = 0
 
 
 if __name__ == "__main__":
-    try_fav_booking()
+    try_in_randomized_time()
     # get_preferred_availability_status()
